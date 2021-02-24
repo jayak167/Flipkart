1e43ea417f463d8ab5c6e212825c05b211dbbceb
jayak167 <jayak167@gmail.com>
2021-02-24 09:05:06 +0530

    Fail case commit

---
+++ src/test/java/Notification/Flipkart2/failcase.java
@@ -0,0 +1,13 @@
+package Notification.Flipkart2;
+
+public final class failcase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String tree = "Banyan";
+
+System.out.println(tree.charAt(7));
+	
+	}
+
+}
